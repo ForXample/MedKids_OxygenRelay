98584047464b4bc847c20f948b868f9765c52190
Zixi Liu <59849551+ForXample@users.noreply.github.com>
2024-01-30 13:59:55 -0500

    [Update] UI logics refined

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,8 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+
+    public GameObject settingsMenu;
+    public GameObject inGameMenu;
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void CloseInGameMenu()
+    {
+        inGameMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit.");
     }
 }
